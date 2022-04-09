75561979121453d48b5100ca83ba93c328fb135f
Cola <bcad1591@gmail.com>
2022-04-09 23:59:40 +0900

    demo

--- src/header/searchBar/autoCompleteStore.js
+++ src/header/searchBar/autoCompleteStore.js
@@ -1,20 +1,28 @@
-const dev = true;
-const apiURL = dev ? 'http://localhost:3000/api/autoComplete' : '';
+const dev = false;
+const apiURL = dev
+  ? 'http://localhost:3000/api/autoComplete'
+  : 'https://raw.githubusercontent.com/codesquad-cola/fe-shopping/main/server/db/autoComplete/autoComplete.json';
 
 export const autoCompleteStore = {
   initialInputKeyword: '',
 
   async requestACKeywords(inputKeyword) {
     try {
-      const response = await fetch(`${apiURL}?q=${inputKeyword}`);
+      const response = await fetch(`${apiURL}`);
 
       if (!response.ok) {
         const bodyText = await response.text();
-        throw new Error(`${response.status} ${response.statusText} ${bodyText}`);
+        throw new Error(
+          `${response.status} ${response.statusText} ${bodyText}`
+        );
       }
 
       const bodyJSON = await response.json();
-      const ACKeywords = Object.values(bodyJSON).map(({ keyword }) => keyword);
+      const parsedData = bodyJSON[inputKeyword] ?? {};
+
+      const ACKeywords = Object.values(parsedData).map(
+        ({ keyword }) => keyword
+      );
       return ACKeywords;
     } catch (err) {
       console.error(err);
